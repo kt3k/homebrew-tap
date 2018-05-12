658f6f61c6af529dbb3203e2454d940a845d4e69
Yoshiya Hinosawa <stibium121@gmail.com>
2018-05-12 14:36:25 +0900

    saku 0.1.2

--- saku.rb
+++ saku.rb
@@ -3,19 +3,20 @@ require "formula"
 class Saku < Formula
   desc "Markdown-based task runner"
   homepage "https://github.com/kt3k/saku"
-  version "v0.1.1"
-  url "TBD"
-  sha256 "TBD"
+  version "v0.1.2"
+  url "https://github.com/kt3k/saku/releases/download/v0.1.2/saku_0.1.2_darwin_amd64.tar.gz"
+  sha256 "7e50d03649e6d55d0cd31f6a6159a86d44c4bc35008159616eafd8af7525cc49"
 
   def install
     bin.install "saku"
   end
 
   def caveats
-    msg = <<- "EOF"
+    msg = <<"EOF"
 
   First create "saku.md".
   See https://github.com/kt3k/saku for details.
 
 EOF
+  end
 end
